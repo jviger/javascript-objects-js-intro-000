173693c1e99f82257ec06b2bfef35e4b299c0474
Jason Viger <jason.viger@gmail.com>
2018-07-09 12:36:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,17 @@
-var playlist = {"Prince",""};
+var playlist = {Weezer:"Buddy Holly"};
+
+var updatePlaylist = function(playlist, artist, song){
+  
+  playlist.artist = song;
+  
+  return playlist;
+  
+}
+
+var removeFromPlaylist = function(playlist, artist){
+  
+ delete playlist.artist;
+  
+  return playlist;
+  
+}
